650555e7f08b7d30618852e1936bea47cce99bad
ty <kim@tyui-MacBookAir.local>
2021-07-29 09:07:22 +0900

    test_alpha_2

--- masters/tests.py
+++ masters/tests.py
@@ -1,3 +1,35 @@
 from django.test import TestCase
+    def setUp(self):
+        ds.object.cret(
+        )
 
-# Create your tests here.
+
+
+
+    def tearDown(self):
+        adsfa.obects.all().delete()
+
+
+
+    def test(self):
+        client = Client()
+
+        reseponse = client.get('  ')
+        print(response.json())
+
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.json(),
+            #  {'message':'SUCCESS',
+
+        'results':[{
+            'gender' : 'asdfasdf',
+            ''
+
+
+
+
+
+
+
+        }]
+
